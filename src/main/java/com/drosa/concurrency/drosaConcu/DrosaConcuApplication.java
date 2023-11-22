aa2672e6b42b39b829fde67e2ca4bda09782bfe5
DAVID ROSA PEINADO <0020143@viewnext.com>
2023-11-22 16:18:38 +0100

    Initial commit

---
+++ src/main/java/com/drosa/concurrency/drosaConcu/DrosaConcuApplication.java
@@ -0,0 +1,13 @@
+package com.drosa.concurrency.drosaConcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrosaConcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrosaConcuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drosa/concurrency/drosaConcu/DrosaConcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drosa.concurrency.drosaConcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrosaConcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
